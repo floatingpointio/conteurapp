385fd555aaf62fb53942f213f072424af5a0904d
Nikica Jokic <neektza@gmail.com>
2016-05-14 19:02:38 +0200

    Refactoring from horrendous to ugly.

--- web/models/event.ex
+++ web/models/event.ex
@@ -18,12 +18,6 @@ defmodule MeetingStories.Event do
   @required_fields ~w(calendar_id origin_id summary status)
   @optional_fields ~w(origin_created_at origin_updated_at starts_at ends_at)
 
-  @doc """
-  Creates a changeset based on the `model` and `params`.
-
-  If no params are provided, an invalid changeset is returned
-  with no validation performed.
-  """
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
