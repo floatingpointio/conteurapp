611b07f4ef850f60c9dbc15f8ec469ce5fff7938
Nikica Jokic <neektza@gmail.com>
2016-06-01 02:42:54 +0200

    Trying to verify with Google.

--- lib/conteur_app/endpoint.ex
+++ lib/conteur_app/endpoint.ex
@@ -9,7 +9,7 @@ defmodule ConteurApp.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :conteur_app, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt googled937389204e5ca42.html)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
